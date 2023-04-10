4b723db6a4bb5b7383768368fad243b0dbaa48ae
Simon <simon@stausholm.info>
2023-04-10 15:41:25 +0200

    setup

--- src/main/java/net/mrsherloque/orespy/mixin/ExampleMixin.java
+++ src/main/java/net/mrsherloque/orespy/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.mrsherloque.orespy.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.mrsherloque.orespy.orespy;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		orespy.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
---
+++ src/main/java/net/mrsherloque/orespy/orespyClient.java
@@ -0,0 +1,11 @@
+package net.mrsherloque.orespy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class orespyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
